3ac6b5d1acaa370d6c9a5a50ed784c80c1c833a5
walter <kolyatamindarov@gmail.com>
2026-03-20 16:42:40 +0300

    test: add Rust unit tests for fastmath module

--- hard/cicd_rust_ci_cd/src/lib.rs
+++ hard/cicd_rust_ci_cd/src/lib.rs
@@ -1,18 +1,30 @@
 use pyo3::prelude::*;
 
+pub fn add_impl(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn multiply_impl(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+pub fn greet_impl(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 #[pyfunction]
 fn add(a: i32, b: i32) -> i32 {
-    a + b
+    add_impl(a, b)
 }
 
 #[pyfunction]
 fn multiply(a: i32, b: i32) -> i32 {
-    a * b
+    multiply_impl(a, b)
 }
 
 #[pyfunction]
 fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+    greet_impl(name)
 }
 
 #[pymodule]
@@ -21,4 +33,32 @@ fn fastmath(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(multiply, m)?)?;
     m.add_function(wrap_pyfunction!(greet, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add_impl(2, 3), 5);
+        assert_eq!(add_impl(-1, 1), 0);
+        assert_eq!(add_impl(0, 0), 0);
+        assert_eq!(add_impl(100, 200), 300);
+    }
+
+    #[test]
+    fn test_multiply() {
+        assert_eq!(multiply_impl(2, 3), 6);
+        assert_eq!(multiply_impl(-2, 3), -6);
+        assert_eq!(multiply_impl(0, 100), 0);
+        assert_eq!(multiply_impl(5, 5), 25);
+    }
+
+    #[test]
+    fn test_greet() {
+        assert_eq!(greet_impl("Rust"), "Hello, Rust!");
+        assert_eq!(greet_impl("World"), "Hello, World!");
+        assert_eq!(greet_impl(""), "Hello, !");
+    }
 }
---
+++ hard/cicd_rust_ci_cd/target/debug/deps/fastmath-6f0804539da759c3.d
@@ -0,0 +1,5 @@
+/home/apollo/cimlov-laba-1/hard/cicd_rust_ci_cd/target/debug/deps/fastmath-6f0804539da759c3.d: src/lib.rs
+
+/home/apollo/cimlov-laba-1/hard/cicd_rust_ci_cd/target/debug/deps/fastmath-6f0804539da759c3: src/lib.rs
+
+src/lib.rs:
